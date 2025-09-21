807718c1f16dccb3c660c9094866933bd928b0ab
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2025-09-21 17:54:28 -0500

    Initial commit

---
+++ src/client/java/net/timeworndevs/final_frontier/FinalFrontierClient.java
@@ -0,0 +1,10 @@
+package net.timeworndevs.final_frontier;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FinalFrontierClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
