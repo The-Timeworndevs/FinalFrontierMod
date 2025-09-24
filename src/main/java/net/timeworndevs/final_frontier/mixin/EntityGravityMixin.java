b26b002c80ff5a6277b0fcecc8720ce228922eb6
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2025-09-24 16:42:51 -0500

    Mixins are fascinating

---
+++ src/main/java/net/timeworndevs/final_frontier/mixin/EntityGravityMixin.java
@@ -0,0 +1,18 @@
+package net.timeworndevs.final_frontier.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.timeworndevs.final_frontier.Main;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public class EntityGravityMixin {
+    @Inject(method = "tick", at = @At("TAIL"))
+    public void tick$GravitySetterMixin(CallbackInfo ci) {
+        while (true) {
+            Main.LOGGER.info("This Mixin is running per entity tick");
+        }
+    }
+}
