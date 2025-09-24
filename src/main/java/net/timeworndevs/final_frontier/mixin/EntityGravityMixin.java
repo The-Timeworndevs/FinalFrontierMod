0f4cc09c20bfd53ff931dab43643c5542b4406f8
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2025-09-24 17:36:21 -0500

    Mixins are confusing

--- src/main/java/net/timeworndevs/final_frontier/mixin/EntityGravityMixin.java
+++ src/main/java/net/timeworndevs/final_frontier/mixin/EntityGravityMixin.java
@@ -1,19 +1,52 @@
 package net.timeworndevs.final_frontier.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.storage.ReadView;
+import net.minecraft.storage.WriteView;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LivingEntity.class)
-public class EntityGravityMixin {
+public class EntityGravityMixin extends Entity {
+
+    public EntityGravityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
     @ModifyReturnValue(method = "getGravity", at = @At("RETURN"))
-    public double getGravity$setGravityChanges(double original) {
+    public static double getGravity$setGravityChanges(double original) {
         return 0.013;
     }
 
     @ModifyReturnValue(method = "getSafeFallDistance()I", at = @At("RETURN"))
-    public int getSafeFallDistance$setFallChanges(int original) {
+    public static int getSafeFallDistance$setFallChanges(int original) {
         return 18;
+    }
+
+    @Override
+    protected void initDataTracker(DataTracker.Builder builder) {
+
+    }
+
+    @Override
+    public boolean damage(ServerWorld world, DamageSource source, float amount) {
+        return false;
+    }
+
+    @Override
+    protected void readCustomData(ReadView view) {
+
+    }
+
+    @Override
+    protected void writeCustomData(WriteView view) {
+
     }
 }
