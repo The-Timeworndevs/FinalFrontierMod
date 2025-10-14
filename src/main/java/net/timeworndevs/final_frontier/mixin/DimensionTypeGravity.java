3e9ffae455490021f68e8180d3941ccd7ccc1d85
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2025-10-14 12:41:37 -0500

    I think this is the right way to d oit?

---
+++ src/main/java/net/timeworndevs/final_frontier/mixin/DimensionTypeGravity.java
@@ -0,0 +1,9 @@
+package net.timeworndevs.final_frontier.mixin;
+
+import java.util.OptionalDouble;
+
+public interface DimensionTypeGravity {
+
+    double gravity();
+    double safeFall();
+}
---
+++ src/main/java/net/timeworndevs/final_frontier/mixin/DimensionTypeGravityMixin.java
@@ -0,0 +1,22 @@
+package net.timeworndevs.final_frontier.mixin;
+
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+import java.util.OptionalDouble;
+
+@Mixin(DimensionType.class)
+public record DimensionTypeGravityMixin(double gravity, double safeFall) implements DimensionTypeGravity {
+
+
+    @Override
+    public double gravity() {
+        return this.gravity();
+    }
+
+    @Override
+    public double safeFall() {
+        return this.safeFall();
+    }
+}
